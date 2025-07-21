5ffe3410ae70715192b7db514d5973d8d2077f82
SUdar <older7740@gmail.com>
2025-07-21 20:22:59 +0300

    Update nerdprogramms.sh

--- nerdprogramms.sh
+++ nerdprogramms.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S waybar rofi git neovim nwg-look wine portproton firefox ramger pavucontrol telegram-desktop libre-office ranger filelight gtop htop swww
+sudo pacman -S waybar rofi git neovim nwg-look wine portproton firefox ramger pavucontrol telegram-desktop libre-office ranger filelight gtop htop swww cava wofi
 #aur
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 #aur-repo
-yay -S vesktop cava wofi thunar 
+yay -S vesktop 
